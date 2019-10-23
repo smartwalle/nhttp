fe2d2c8a66e91c0b87561d3d53962829f0afebd8
smartwalle <smartwalle@gmail.com>
2019-10-23 10:58:43 +0800

    update demo

--- sample/http/gate.go
+++ sample/http/gate.go
@@ -3,6 +3,7 @@ package main
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/smartwalle/http4go"
+	"net/http"
 	"net/url"
 )
 
@@ -10,16 +11,28 @@ func main() {
 	var s = gin.Default()
 	var bufferPool = http4go.NewBufferPool(1024)
 
+	var targets = make(map[string]*url.URL)
+	var tURL *url.URL
+
+	tURL, _ = url.Parse("http://127.0.0.1:8891?gate=1")
+	targets["user"] = tURL
+
+	tURL, _ = url.Parse("http://127.0.0.1:8892?gate=1")
+	targets["order"] = tURL
+
 	var rp = http4go.NewReverseProxy(bufferPool)
 
-	s.Any("/api/user/*path", func(c *gin.Context) {
-		var u, _ = url.Parse("http://127.0.0.1:8891?c=2")
-		rp.ProxyWithURL(u).ServeHTTP(c.Writer, c.Request)
-	})
+	s.Any("/api/:server/*path", func(c *gin.Context) {
+		var server = c.Param("server")
 
-	s.Any("/api/order/*path", func(c *gin.Context) {
-		var u, _ = url.Parse("http://127.0.0.1:8892?c=2")
-		rp.ProxyWithURL(u).ServeHTTP(c.Writer, c.Request)
+		var target = targets[server]
+		if target == nil {
+			c.AbortWithStatus(http.StatusBadGateway)
+			return
+		}
+
+		var p = rp.ProxyWithURL(target)
+		p.ServeHTTP(c.Writer, c.Request)
 	})
 
 	s.Run(":8890")
