16950edd8527fa5cb3cdaef2458f7fdad6a25692
smartwalle <smartwalle@gmail.com>
2023-06-09 09:42:36 +0800

    remove reverseproxy

--- log.go
+++
@@ -1,9 +0,0 @@
-package nhttp
-
-type Logger interface {
-	SetPrefix(prefix string)
-	Prefix() string
-	Println(args ...interface{})
-	Printf(format string, args ...interface{})
-	Output(calldepth int, s string) error
-}
