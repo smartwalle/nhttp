4669bef68856067d97f99fa169973a2f07e506b0
smartwalle <smartwalle@gmail.com>
2023-07-05 10:13:54 +0800

    Mapper

---
+++ mapper.go
@@ -0,0 +1,305 @@
+package nhttp
+
+import (
+	"errors"
+	"reflect"
+	"strconv"
+	"sync"
+	"sync/atomic"
+)
+
+const (
+	kNoTag = "-"
+	kTag   = "form"
+)
+
+type fieldDescriptor struct {
+	Index []int
+	Tag   string
+}
+
+type structDescriptor struct {
+	Fields []fieldDescriptor
+}
+
+type Mapper struct {
+	tag     string
+	structs atomic.Value // map[reflect.Type]structDescriptor
+	mu      sync.Mutex
+}
+
+func NewMapper() *Mapper {
+	var m = &Mapper{}
+	m.tag = kTag
+	m.structs.Store(make(map[reflect.Type]structDescriptor))
+	return m
+}
+
+func (this *Mapper) Bind(src map[string][]string, dst interface{}) error {
+	var dstValue = reflect.ValueOf(dst)
+	var dstType = dstValue.Type()
+
+	if dstValue.Kind() != reflect.Ptr {
+		return errors.New("must pass a pointer to Bind")
+	}
+
+	if dstValue.IsNil() {
+		return errors.New("nil pointer passed to Bind")
+	}
+
+	for {
+		if dstValue.Kind() == reflect.Ptr && dstValue.IsNil() {
+			dstValue.Set(reflect.New(dstType.Elem()))
+		}
+
+		if dstValue.Kind() == reflect.Ptr {
+			dstValue = dstValue.Elem()
+			dstType = dstType.Elem()
+			continue
+		}
+		break
+	}
+
+	var dStruct, ok = this.getStructDescriptor(dstType)
+	if !ok {
+		dStruct = this.parseStructDescriptor(dstType)
+	}
+
+	for _, field := range dStruct.Fields {
+		var values, ok = src[field.Tag]
+		if !ok {
+			continue
+		}
+
+		var fieldValue = fieldByIndex(dstValue, field.Index)
+		if err := mapValues(fieldValue, values); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func fieldByIndex(v reflect.Value, index []int) reflect.Value {
+	if len(index) == 1 {
+		return v.Field(index[0])
+	}
+	for i, x := range index {
+		if i > 0 {
+			if v.Kind() == reflect.Pointer && v.Type().Elem().Kind() == reflect.Struct {
+				if v.IsNil() {
+					v.Set(reflect.New(v.Type().Elem()))
+				}
+				v = v.Elem()
+			}
+		}
+		v = v.Field(x)
+	}
+	return v
+}
+
+func (this *Mapper) getStructDescriptor(key reflect.Type) (structDescriptor, bool) {
+	var value, ok = this.structs.Load().(map[reflect.Type]structDescriptor)[key]
+	return value, ok
+}
+
+func (this *Mapper) setStructDescriptor(key reflect.Type, value structDescriptor) {
+	var structs = this.structs.Load().(map[reflect.Type]structDescriptor)
+	structs[key] = value
+	this.structs.Store(structs)
+}
+
+type structQueueElement struct {
+	Type  reflect.Type
+	Index []int
+}
+
+func (this *Mapper) parseStructDescriptor(dstType reflect.Type) structDescriptor {
+	this.mu.Lock()
+
+	var dStruct, ok = this.getStructDescriptor(dstType)
+	if ok {
+		this.mu.Unlock()
+		return dStruct
+	}
+
+	var queue = make([]structQueueElement, 0, 3)
+	queue = append(queue, structQueueElement{
+		Type:  dstType,
+		Index: nil,
+	})
+
+	var dFields = make(map[string]fieldDescriptor)
+
+	for len(queue) > 0 {
+		var current = queue[0]
+		queue = queue[1:]
+
+		var numField = current.Type.NumField()
+
+		for i := 0; i < numField; i++ {
+			var fieldStruct = current.Type.Field(i)
+
+			var tag = fieldStruct.Tag.Get(this.tag)
+			if tag == kNoTag {
+				continue
+			}
+
+			if tag == "" {
+				tag = fieldStruct.Name
+
+				if fieldStruct.Type.Kind() == reflect.Ptr {
+					queue = append(queue, structQueueElement{
+						Type:  fieldStruct.Type.Elem(),
+						Index: append(current.Index, i),
+					})
+					continue
+				}
+
+				if fieldStruct.Type.Kind() == reflect.Struct {
+					queue = append(queue, structQueueElement{
+						Type:  fieldStruct.Type,
+						Index: append(current.Index, i),
+					})
+					continue
+				}
+			}
+
+			if _, exists := dFields[tag]; exists {
+				continue
+			}
+
+			var dField = fieldDescriptor{}
+			dField.Index = append(current.Index, i)
+			dField.Tag = tag
+
+			dFields[tag] = dField
+		}
+	}
+
+	dStruct.Fields = make([]fieldDescriptor, 0, len(dFields))
+	for _, field := range dFields {
+		dStruct.Fields = append(dStruct.Fields, field)
+	}
+
+	this.setStructDescriptor(dstType, dStruct)
+	this.mu.Unlock()
+
+	return dStruct
+}
+
+func mapValues(field reflect.Value, values []string) error {
+	if field.Kind() == reflect.Slice /* && field.IsNil() == false */ {
+		var valueLen = 1
+		if len(values) > 1 {
+			// 如果绑定源数据也是 slice
+			var s = reflect.MakeSlice(field.Type(), valueLen, valueLen)
+			for i := 0; i < valueLen; i++ {
+				if err := mapValue(s.Index(i), values[i]); err != nil {
+					return err
+				}
+			}
+			field.Set(s)
+		} else {
+			// 如果绑定源数据不是 slice
+			var s = reflect.MakeSlice(field.Type(), valueLen, valueLen)
+			if err := mapValue(s.Index(0), values[0]); err != nil {
+				return err
+			}
+			field.Set(s)
+		}
+		return nil
+	}
+	return mapValue(field, values[0])
+}
+
+func mapValue(field reflect.Value, value string) error {
+	switch field.Kind() {
+	case reflect.Interface:
+		field.Set(reflect.ValueOf(value))
+	case reflect.String:
+		field.SetString(value)
+	case reflect.Int:
+		return mapInt(field, value, 0)
+	case reflect.Int8:
+		return mapInt(field, value, 8)
+	case reflect.Int16:
+		return mapInt(field, value, 16)
+	case reflect.Int32:
+		return mapInt(field, value, 32)
+	case reflect.Int64:
+		return mapInt(field, value, 64)
+	case reflect.Uint:
+		return mapUint(field, value, 0)
+	case reflect.Uint8:
+		return mapUint(field, value, 8)
+	case reflect.Uint16:
+		return mapUint(field, value, 16)
+	case reflect.Uint32:
+		return mapUint(field, value, 32)
+	case reflect.Uint64:
+		return mapUint(field, value, 64)
+	case reflect.Uintptr:
+		return mapUint(field, value, 64)
+	case reflect.Float32:
+		return mapFloat(field, value, 32)
+	case reflect.Float64:
+		return mapFloat(field, value, 64)
+	case reflect.Bool:
+		return mapBool(field, value)
+	default:
+		return errors.New("cannot unmarshal into " + field.Type().String())
+	}
+	return nil
+}
+
+func mapInt(field reflect.Value, value string, bitSize int) error {
+	if value == "" {
+		field.SetInt(0)
+		return nil
+	}
+	intValue, err := strconv.ParseInt(value, 10, bitSize)
+	if err != nil {
+		return err
+	}
+	field.SetInt(intValue)
+	return nil
+}
+
+func mapUint(field reflect.Value, value string, bitSize int) error {
+	if value == "" {
+		field.SetUint(0)
+		return nil
+	}
+	intValue, err := strconv.ParseUint(value, 10, bitSize)
+	if err != nil {
+		return err
+	}
+	field.SetUint(intValue)
+	return nil
+}
+
+func mapFloat(field reflect.Value, value string, bitSize int) error {
+	if value == "" {
+		field.SetFloat(0)
+		return nil
+	}
+	intValue, err := strconv.ParseFloat(value, bitSize)
+	if err != nil {
+		return err
+	}
+	field.SetFloat(intValue)
+	return nil
+}
+
+func mapBool(field reflect.Value, value string) error {
+	if value == "" {
+		field.SetBool(false)
+		return nil
+	}
+	booleValue, err := strconv.ParseBool(value)
+	if err != nil {
+		return err
+	}
+	field.SetBool(booleValue)
+	return nil
+}
---
+++ mapper_test.go
@@ -0,0 +1,147 @@
+package nhttp_test
+
+import (
+	"github.com/smartwalle/nhttp"
+	"net/url"
+	"testing"
+)
+
+type Human struct {
+	Name     string `form:"name"`
+	Age      int    `form:"age"`
+	Birthday string `form:"birthday"`
+}
+
+func TestMapper_BindHuman(t *testing.T) {
+	var tests = []struct {
+		form  url.Values
+		human Human
+	}{
+		{
+			form:  url.Values{"name": {"name1"}, "age": {"10"}, "birthday": {"2023-07-05"}},
+			human: Human{Name: "name1", Age: 10, Birthday: "2023-07-05"},
+		},
+		{
+			form:  url.Values{"name": {"name2"}, "age": {"11"}, "birthday": {"2023-07-06"}},
+			human: Human{Name: "name2", Age: 11, Birthday: "2023-07-06"},
+		},
+		{
+			form:  url.Values{"name": {"name3"}, "age": {"0"}, "birthday": {"2023-07-07"}},
+			human: Human{Name: "name3", Age: 0, Birthday: "2023-07-07"},
+		},
+		{
+			form:  url.Values{"name": {"name4"}, "age": {"-1"}, "birthday": {"2023-07-08"}},
+			human: Human{Name: "name4", Age: -1, Birthday: "2023-07-08"},
+		},
+
+		{
+			form:  url.Values{"name": {"name5"}, "age": {""}, "birthday": {"2023-07-09"}},
+			human: Human{Name: "name5", Age: 0, Birthday: "2023-07-09"},
+		},
+	}
+
+	var m = nhttp.NewMapper()
+
+	for _, test := range tests {
+		var dst Human
+
+		if err := m.Bind(test.form, &dst); err != nil {
+			t.Fatal(err)
+		}
+
+		if dst.Name != test.human.Name {
+			t.Fatalf("Name - 期望: %s, 实际: %s", test.human.Name, dst.Name)
+		}
+
+		if dst.Age != test.human.Age {
+			t.Fatalf("Age - 期望: %d, 实际: %d", test.human.Age, dst.Age)
+		}
+
+		if dst.Birthday != test.human.Birthday {
+			t.Fatalf("Age - 期望: %s, 实际: %s", test.human.Birthday, dst.Birthday)
+		}
+	}
+}
+
+type Student struct {
+	Human
+	Number int64  `form:"number"`
+	Class  string `form:"class"`
+}
+
+var studentForm = url.Values{"name": {"Yangfeng"}, "age": {"10"}, "number": {"3414416614257328130"}, "birthday": {"2016-06-12"}, "class": {"class"}}
+
+func TestMapper_Bind(t *testing.T) {
+	var s *Student
+	var m = nhttp.NewMapper()
+	m.Bind(studentForm, &s)
+	t.Log(s)
+}
+
+func BenchmarkMapperBind(b *testing.B) {
+	var m = nhttp.NewMapper()
+	b.ReportAllocs()
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		var s Student
+		m.Bind(studentForm, &s)
+	}
+	b.StopTimer()
+}
+
+var userForm = url.Values{"firstname": {"Feng"}, "lastname": {"Yang"}, "email": {"yangfeng@qq.com"}, "age": {"10"}}
+
+type User struct {
+	Firstname string `form:"firstname"`
+	Lastname  string `form:"lastname"`
+	Email     string `form:"email"`
+	Age       int    `form:"age"`
+}
+
+func BenchmarkMapperBind_Struct(b *testing.B) {
+	var m = nhttp.NewMapper()
+	b.ReportAllocs()
+	b.ResetTimer()
+
+	for n := 0; n < b.N; n++ {
+		var u User
+		m.Bind(userForm, &u)
+	}
+}
+
+func BenchmarkMapperBind_Pointer(b *testing.B) {
+	var m = nhttp.NewMapper()
+	b.ReportAllocs()
+	b.ResetTimer()
+
+	for n := 0; n < b.N; n++ {
+		var u *User
+		m.Bind(userForm, &u)
+	}
+}
+
+func BenchmarkMapperBind_StructParallel(b *testing.B) {
+	var m = nhttp.NewMapper()
+	b.ReportAllocs()
+	b.ResetTimer()
+
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			var u User
+			m.Bind(userForm, &u)
+		}
+	})
+}
+
+func BenchmarkMapperBind_PointerParallel(b *testing.B) {
+	var m = nhttp.NewMapper()
+	b.ReportAllocs()
+	b.ResetTimer()
+
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			var u *User
+			m.Bind(userForm, &u)
+		}
+	})
+}
